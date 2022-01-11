a9b7e9ed024a9fa3349cc6b5a447ab27b1aba03f
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-11 15:28:25 +0100

    Aggiunto components header

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+
+        <div>
+            LOGO
+        </div>
+
+        <div class="input-container">
+            <input type="text" placeholder="Cerca un film">
+            <button>Cerca</button>
+        </div>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang='scss' scoped>
+
+    header {
+        background-color: black;
+        color: white;
+        display: flex;
+        justify-content: space-around;
+        padding: 20px 0;
+
+        .input-container {
+
+            input[type=text] {
+                margin-right: 5px;
+            }
+
+            button {
+                cursor: pointer;
+            }
+        }
+    }
+</style>
